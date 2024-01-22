26e6db8b47696a3433b37ece41328d10e577e08d
Shokhanasser <shokhanasserprojr@gmail.com>
2024-01-22 23:16:23 +0500

    delete css and create sass

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss'
 import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and see the magic!.
         </p>
