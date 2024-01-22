8958b80fce13ba6eaccc8adb6f543d83005e4b3f
Shokhanasser <shokhanasserprojr@gmail.com>
2024-01-23 01:01:28 +0500

    Component About

--- travel/src/components/About/index.jsx
+++ travel/src/components/About/index.jsx
@@ -1,7 +1,19 @@
-
+import './style.scss'
 function About() {
     return (
-        <h2>Hello I'm Shokhanasser. I'm Fullstack developer.</h2>
+        <div id='about-wrapper'>
+            <div className='bgImage'>
+                <div className="about-text">
+                    <h2>О нас</h2>
+                    <p>PeroTravel - Первый онлайн-сервис по бронированию экскурсий без очередей и операторов. <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
