8744e5e9ad1b15705109ad4c7084dce19c063374
Shokhanasser <shokhanasserprojr@gmail.com>
2024-01-28 04:20:40 +0500

    about media

--- travel/src/components/common/Carousel/index.jsx
+++ travel/src/components/common/Carousel/index.jsx
@@ -7,16 +7,16 @@ export default function BlogProductCarousel() {
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
+            breakpoint: { max: 1920, min: 1024},
+            items: 3
         },
         desktop: {
-            breakpoint: { max: 3000, min: 768 },
+            breakpoint: { max: 1024, min: 768 },
             items: 3
         },
         tablet: {
             breakpoint: { max: 768, min: 474 },
-            items: 2
+            items: 3
         },
         mobile: {
             breakpoint: { max: 474, min: 0 },
@@ -40,6 +40,7 @@ export default function BlogProductCarousel() {
                         <button className="btn">Подробнее</button>
 
                     </div>
+                    
                     <div className="card-2">
                         <span className="name-2">Конный тур</span>
                         <h4 className="name--3">Конные прогулки</h4>
@@ -66,6 +67,32 @@ export default function BlogProductCarousel() {
                         <button className="btn">Подробнее</button>
 
                     </div>
+                    <div className="card-1">
+                        <span className="name-1">Автобусный тур</span>
+                        <h4 className="name--2">Обзорная по Сочи (из Адлера)</h4>
+                        <small className="time-1">6 часов</small> <small className="price-1">618рублей</small>
+                        <p className="info-1">Приглашаем вас на экскурсию
+                            "Обзорная по Большому Сочи":
+                            Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...</p>
+                        <button className="btn">Подробнее</button>
+
+                    </div>
+                    <div className="card-2">
+                        <span className="name-2">Конный тур</span>
+                        <h4 className="name--3">Конные прогулки</h4>
+                        <small className="time-2">1,5 часов</small> <small className="price-2">1809 рублей</small>
+                        <p className="info-2">Конные прогулки проходят в Лазурной долине. С долины открывается красивый вид на море, горы и лес. На всем пути следования экскурсии вас будут сопровождать опытные инструктора. Катание на лошадях или иппотерапия очень полезна для детей и взрослых...</p>
+                        <button className="btn">Подробнее</button>
+                    </div>
+                    <div className="card-3">
+                        <span className="name-3">Квадротур</span>
+                        <h4 className="name--4">Пасть дракона</h4>
+                        <small className="time-3">2,5 часов</small> <small className="price-3">3515 рублей</small>
+                        <p className="info-3">Маршрут на Квадроциклах проходит по горной реке Мзымта вдоль белых скал и самшитовый лес к водопаду Глубокий Яр или "Пасть Дракона" (экскурсионное название).
+Высота водопада 41,5 метр - это один из самых высоких водопадов Большого Сочи...</p>
+                        <button className="btn">Подробнее</button>
+                    </div>
+
                 </Carousel>
             </div>
 
