e6275f2b0ed5486b42d14999f2af8d9065fe9442
Shokhanasser <shokhanasserprojr@gmail.com>
2024-01-30 06:56:04 +0500

    last commit part 2

--- travel/src/components/Third-page/thirdGallery.jsx
+++ travel/src/components/Third-page/thirdGallery.jsx
@@ -1,55 +1,37 @@
-import './style.scss';
- 
+import './galleryStyle.scss'
+
+import React from 'react'
 import gallery1 from '../../assets/images/1.png'
-import gallery2 from '../../assets/images/1.png'
-import gallery3 from '../../assets/images/1.png'
-import gallery4 from '../../assets/images/1.png'
-import gallery5 from '../../assets/images/1.png'
-import gallery6 from '../../assets/images/1.png'
-import gallery7 from '../../assets/images/1.png'
-import gallery8 from '../../assets/images/1.png'
+import gallery2 from '../../assets/images/2.png'
+import gallery3 from '../../assets/images/3.png'
+import gallery4 from '../../assets/images/4.png'
+import gallery5 from '../../assets/images/5.png'
+import gallery6 from '../../assets/images/6.png'
+import gallery7 from '../../assets/images/7.png'
+import gallery8 from '../../assets/images/8.png'
+export default function GalleryDiv() {
+  return (
+    <div className="gallery-div">
+        <div className="left">
+            <div className="top-img-1"><img src={gallery1} alt="" /></div>
+            <div className="bottom">
+                <div className="left-img-2"><img src={gallery5} alt="" /></div>
+                <div className="m-right">
+                    <div className="top-img-3"><img src={gallery6} alt="" /></div>
+                    <div className="bottom-img-4"><img src={gallery7} alt="" /></div>
+                </div>
+            </div>
+        </div>
 
- 
+        <div className="middle-img-5"><img src={gallery2} alt="" /></div>
 
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import { Link } from 'react-router-dom';
-
-  
-
-
-
-function ThirdGallery() {
-
- 
- //=========================================================================================
-    const images = [
-        gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8
-    ]
-    return (
-        <div className="gallery">
-
-            <h2>Галерея</h2>
-            <span><Link to={'/'}>Смотреть все</Link></span>
-
-            <ResponsiveMasonry
-                columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}
-            >
-                <Masonry>
-                    {images.map((image, i) => (
-                        <img
-                            key={i}
-                            src={image}
-                            style={{ width: "100%", height: "70%", display: "block", padding: "8px" }}
-                            alt=""
-                        />
-                    ))}
-                </Masonry>
-            </ResponsiveMasonry>
-
-
- 
-         </div>
-    );
+        <div className="right">
+            <div className="r-top">
+                <div className="left-img-6"><img src={gallery3} alt="" /></div>
+                <div className="right-img-7"><img src={gallery4} alt="" /></div>
+            </div>
+            <div className="bottom-img-8"><img src={gallery8} alt="" /></div>
+        </div>
+    </div>
+  )
 }
-
-export default ThirdGallery;
